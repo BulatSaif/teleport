d7693bf961ada489205fa130bae5ea98c037c036
Paul Gottschling <paul.gottschling@goteleport.com>
2024-06-20 12:24:02 -0400

    Failing test for redirect config

--- docs/check-redirects/check-redirects.test.js
+++ docs/check-redirects/check-redirects.test.js
@@ -22,27 +22,34 @@ Here is a link to a [missing page](https://goteleport.com/docs/page3).
     '/docs/content/1.x/docs/pages/page1.mdx': `---
 title: "Sample Page 1"
 ---
-
-This is a link to a [documentation page](https://goteleport.com/docs/page1).
-
 `,
     '/docs/content/1.x/docs/pages/subdirectory/page2.mdx': `---
 title: "Sample Page 2"
 ---
-
-This is a link to a [documentation page](https://goteleport.com/docs/page2).
-
 `,
-  }
+  };
 
   test(`throws an error if there is no redirect for a missing docs page`, () => {
     const vol = Volume.fromJSON(files);
     const fs = createFsFromVolume(vol);
     const checker = new RedirectChecker(fs, '/blog', '/docs/content/1.x', []);
     expect(() => {
-    	checker.check();
+      checker.check();
     }).toThrow('https://goteleport.com/docs/page3');
   });
 
-    // TODO: Same test case as above, but with a redirect
+  test(`allows missing docs pages if there is a redirect`, () => {
+    const vol = Volume.fromJSON(files);
+    const fs = createFsFromVolume(vol);
+    const checker = new RedirectChecker(fs, '/blog', '/docs/content/1.x', [
+      {
+        source: '/docs/page3/',
+        destination: '/docs/page1',
+        permanent: true,
+      },
+    ]);
+    expect(() => {
+      checker.check();
+    }).not.toThrow('https://goteleport.com/docs/page3');
+  });
 });
