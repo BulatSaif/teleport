1c3a8bf0ea9efb3965d151c9a9f55c2651e702cf
Paul Gottschling <paul.gottschling@goteleport.com>
2024-06-20 10:33:34 -0400

    Flesh out the test

--- docs/check-redirects/check-redirects.js
+++ docs/check-redirects/check-redirects.js
@@ -5,14 +5,18 @@ const path = require('path');
 // given file tree and determines whether a given docs configuration requires
 // redirects.
 // @param fs - The filesystem to use. Either memfs or the NodeJS fs package.
-// @param {string} root - file path in fs in which to check for required
-// redirects.
+// @param {string} otherRepoRoot - directory path in fs in which to check for
+// required redirects.
+// @param {string} docsRoot - directory path in fs in which to check for present
+// or missing docs files based on URL paths found in the directory tree at 
+// otherRepoRoot.
 // @param {object} redirects - array of objects with keys "source",
 // "destination", and "permanent".
 class RedirectChecker {
-  constructor(fs, root, redirects) {
+  constructor(fs, otherRepoRoot, docsRoot, redirects) {
     this.fs = fs;
-    this.root = root;
+    this.otherRepoRoot = otherRepoRoot;
+    this.docsRoot = docsRoot;
     this.redirects = redirects;
   }
 }
--- docs/check-redirects/check-redirects.test.js
+++ docs/check-redirects/check-redirects.test.js
@@ -2,31 +2,47 @@ import { Volume, createFsFromVolume } from 'memfs';
 import { RedirectChecker } from './check-redirects.js';
 
 describe('check files for links to missing Teleport docs', () => {
-  const fileTree = {
-    '/page1.mdx': `---
+  const files = {
+    '/blog/content1.mdx': `---
 title: "Sample Page 1"
 ---
 
 This is a link to a [documentation page](https://goteleport.com/docs/page1).
 
 `,
-    '/page2.mdx': `---
+    '/blog/content2.mdx': `---
+title: "Sample Page 2"
+---
+
+This is a link to a [documentation page](https://goteleport.com/docs/subdirectory/page2).
+
+Here is a link to a [missing page](https://goteleport.com/docs/page3.mdx).
+
+`,
+    '/docs/content/1.x/docs/pages/page1.mdx': `---
+title: "Sample Page 1"
+---
+
+This is a link to a [documentation page](https://goteleport.com/docs/page1).
+
+`,
+    '/docs/content/1.x/docs/pages/subdirectory/page2.mdx': `---
 title: "Sample Page 2"
 ---
 
 This is a link to a [documentation page](https://goteleport.com/docs/page2).
 
 `,
-  };
+  }
 
   test(`throws an error if there is no redirect for a missing docs page`, () => {
-    // TODO: Feed a filesystem with current docs pages to the checker
-    const vol = Volume.fromJSON(fileTree);
+    const vol = Volume.fromJSON(files);
     const fs = createFsFromVolume(vol);
+    const checker = new RedirectChecker(fs, '/blog', '/docs/content/1.x', []);
     expect(() => {
-      const frag = new RedirectChecker(fs, '/docs');
-      // TODO: Add a function to run the check.
-      // TODO: Add an expected error substring
-    }).toThrow('');
+    	checker.check();
+    }).toThrow(/content2\.mdx.*page2.mdx/);
   });
+
+    // TODO: Same test case as above, but with a redirect
 });
